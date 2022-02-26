744b9115a0cc22e28dc82ec849d10b9cb3fc1f60
GaelPouxMedard <gael.poux-medard@univ-lyon2.fr>
2022-02-26 14:16:18 +0100

    Updates...

--- MPDHP.py
+++ MPDHP.py
@@ -337,12 +337,12 @@ class Dirichlet_Hawkes_Process(object):
 			remaining_files = []
 			for remaining_particle in remaining_particles:
 				for file in remaining_particle.files_clusters:
-					remaining_files.append(file)
+					remaining_files.append(file[1])
 			for removed_particle in removed_particles:
 				for file in removed_particle.files_clusters:
-					if file not in remaining_files:
+					if file[1] not in remaining_files[1]:
 						os.remove(file[1])  # file[0] = index
-						remaining_files.append(file)
+						remaining_files.append(file[1])
 
 			resample_probs = weights[np.where(weights + 1e-5 > threshold)]
 			resample_probs = resample_probs/np.sum(resample_probs)
